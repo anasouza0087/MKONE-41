5c8629e84c42bf43288f2c6b1a22b4fa9915a3a6
Ana <ana.souza0087@gmail.com>
2024-09-26 16:29:59 -0300

    feat: add image upload function

--- spyke-editors/src/app/Editors/React-Email-Editor/ReactEmailEditor.tsx
+++ spyke-editors/src/app/Editors/React-Email-Editor/ReactEmailEditor.tsx
@@ -2,79 +2,238 @@ import React, { useRef } from "react";
 
 import EmailEditor, { EditorRef, EmailEditorProps } from "react-email-editor";
 
+const jsonMock = {
+  counters: {
+    u_column: 1,
+    u_row: 1,
+    u_content_button: 1,
+  },
+  body: {
+    id: "tQMIFyElgm",
+    rows: [
+      {
+        id: "G3Uc4CO-ex",
+        cells: [1],
+        columns: [
+          {
+            id: "J2O0qFiZWF",
+            contents: [
+              {
+                id: "Nqo5p8BlGx",
+                type: "button",
+                values: {
+                  href: {
+                    name: "web",
+                    values: {
+                      href: "",
+                      target: "_blank",
+                    },
+                  },
+                  buttonColors: {
+                    color: "#FFFFFF",
+                    backgroundColor: "#3AAEE0",
+                    hoverColor: "#FFFFFF",
+                    hoverBackgroundColor: "#3AAEE0",
+                  },
+                  size: {
+                    autoWidth: true,
+                    width: "100%",
+                  },
+                  fontSize: "14px",
+                  lineHeight: "120%",
+                  textAlign: "center",
+                  padding: "10px 20px",
+                  border: {},
+                  borderRadius: "4px",
+                  displayCondition: null,
+                  _styleGuide: null,
+                  containerPadding: "10px",
+                  anchor: "",
+                  _meta: {
+                    htmlID: "u_content_button_1",
+                    htmlClassNames: "u_content_button",
+                  },
+                  selectable: true,
+                  draggable: true,
+                  duplicatable: true,
+                  deletable: true,
+                  hideable: true,
+                  text: '<span style="line-height: 16.8px;">Button Text</span>',
+                  _languages: {},
+                  calculatedWidth: 110,
+                  calculatedHeight: 37,
+                },
+              },
+            ],
+            values: {
+              backgroundColor: "",
+              padding: "0px",
+              border: {},
+              borderRadius: "0px",
+              _meta: {
+                htmlID: "u_column_1",
+                htmlClassNames: "u_column",
+              },
+            },
+          },
+        ],
+        values: {
+          displayCondition: null,
+          columns: false,
+          _styleGuide: null,
+          backgroundColor: "",
+          columnsBackgroundColor: "",
+          backgroundImage: {
+            url: "",
+            fullWidth: true,
+            repeat: "no-repeat",
+            size: "custom",
+            position: "center",
+            customPosition: ["50%", "50%"],
+          },
+          padding: "0px",
+          anchor: "",
+          hideDesktop: false,
+          _meta: {
+            htmlID: "u_row_1",
+            htmlClassNames: "u_row",
+          },
+          selectable: true,
+          draggable: true,
+          duplicatable: true,
+          deletable: true,
+          hideable: true,
+        },
+      },
+    ],
+    headers: [],
+    footers: [],
+    values: {
+      _styleGuide: null,
+      popupPosition: "center",
+      popupWidth: "600px",
+      popupHeight: "auto",
+      borderRadius: "10px",
+      contentAlign: "center",
+      contentVerticalAlign: "center",
+      contentWidth: "500px",
+      fontFamily: {
+        label: "Arial",
+        value: "arial,helvetica,sans-serif",
+      },
+      textColor: "#000000",
+      popupBackgroundColor: "#FFFFFF",
+      popupBackgroundImage: {
+        url: "",
+        fullWidth: true,
+        repeat: "no-repeat",
+        size: "cover",
+        position: "center",
+      },
+      popupOverlay_backgroundColor: "rgba(0, 0, 0, 0.1)",
+      popupCloseButton_position: "top-right",
+      popupCloseButton_backgroundColor: "#DDDDDD",
+      popupCloseButton_iconColor: "#000000",
+      popupCloseButton_borderRadius: "0px",
+      popupCloseButton_margin: "0px",
+      popupCloseButton_action: {
+        name: "close_popup",
+        attrs: {
+          onClick:
+            "document.querySelector('.u-popup-container').style.display = 'none';",
+        },
+      },
+      language: {},
+      backgroundColor: "#F7F8F9",
+      preheaderText: "",
+      linkStyle: {
+        body: true,
+        linkColor: "#0000ee",
+        linkHoverColor: "#0000ee",
+        linkUnderline: true,
+        linkHoverUnderline: true,
+      },
+      backgroundImage: {
+        url: "",
+        fullWidth: true,
+        repeat: "no-repeat",
+        size: "custom",
+        position: "center",
+      },
+      _meta: {
+        htmlID: "u_body",
+        htmlClassNames: "u_body",
+      },
+    },
+  },
+  schemaVersion: 17,
+};
+
 export const ReactEmailEditor = (props: any) => {
   const emailEditorRef = useRef<EditorRef>(null);
 
-  // const exportHtml = () => {
-  //   const unlayer = emailEditorRef.current?.editor;
+  const exportHtml = () => {
+    const unlayer = emailEditorRef.current?.editor;
 
-  //   unlayer?.exportHtml((data) => {
-  //     const { design, html } = data;
-  //     console.log("exportHtml", html);
-  //   });
-  // };
-
-  // const onLoad = () => {
-  //   const unlayer = emailEditorRef.current?.editor;
-
-  //   // Chame init para personalizar ferramentas como o botão
-  //   unlayer?.init({
-  //     tools: {
-  //       button: {
-  //         properties: {
-  //           text: {
-  //             defaultValue: "Clica aqui", // Defina o valor padrão do texto do botão
-  //           }
-  //         }
-  //       }
-  //     }
-  //   });
-
-  //   // Definindo um design inicial para o editor, com as propriedades corretas
-  //   const initialDesign = {
-  //     id: "initial-design-id", // ID obrigatório
-  //     body: {
-  //       id: "body-id", // ID do corpo
-  //       rows: [], // Linhas do design
-  //       headers: [], // Cabeçalhos (pode ser vazio)
-  //       footers: [], // Rodapés (pode ser vazio)
-  //       values: {} // Propriedades gerais
-  //     },
-  //     counters: {
-  //       u_row: 1,
-  //       u_column: 1,
-  //       u_content: 1
-  //     }
-  //   };
-
-  //   // Carregando o design inicial
-  //   unlayer?.loadDesign(initialDesign);
-  // };
+    unlayer?.exportHtml((data) => {
+      const { design, html } = data;
+    });
+  };
 
   const onReady: EmailEditorProps["onReady"] = (unlayer) => {
-    //   console.log(unlayer);
-    //   // editor is ready
-    //   // you can load your template here;
-    //   // the design json can be obtained by calling
-    //   // unlayer.loadDesign(callback) or unlayer.exportHtml(callback)
-    //   // const templateJson = { DESIGN JSON GOES HERE };
-    //   // unlayer.loadDesign(templateJson);
+    unlayer.loadDesign(jsonMock);
 
-    
+    unlayer.registerCallback("image", function (file: any, done: any) {
+      var data = new FormData();
+      data.append("file", file.attachments[0]);
+      console.log(file);
+      console.log(file.attachments[0]);
+      const object = window.URL.createObjectURL(file.attachments[0]);
+      done({
+        progress: 100,
+        url: "https://www.futbox.com/img/v1/9c4/4a0/3c5/e3f/e8ef093be708a0aabbf3.png",
+      });
+      // fetch("/uploads", {
+      //   method: "POST",
+      //   headers: {
+      //     Accept: "application/json",
+      //   },
+      //   body: data,
+      // })
+      //   .then((response) => {
+      //     // Make sure the response was valid
+      //     if (response.status >= 200 && response.status < 300) {
+      //       return response;
+      //     } else {
+      //       var error = new Error(response.statusText);
+      //       // error.response = response
+      //       throw error;
+      //     }
+      //   })
+      //   .then((response) => {
+      //     return response.json();
+      //   })
+      //   .then((data) => {
+      //     // Pass the URL back to Unlayer to mark this upload as completed
+      //     done({ progress: 100, url: data.filelink });
+      //   });
+    });
   };
 
   return (
     <div>
-      <div>{/* <button onClick={exportHtml}>Export HTML</button> */}</div>
+      <div>
+        <button onClick={exportHtml}>Export HTML</button>
+      </div>
 
       <EmailEditor
+        style={{ height: "100vh" }}
         ref={emailEditorRef}
-        // onLoad={onLoad}
         onReady={onReady}
         options={{
           locale: "pt-BR",
           appearance: {
-            // Definição de temas (padrão). Mas, pode ser customizável.
+            // Definição de temas (padrão). Mas, pode ser customizável (versão premium).
             theme: "classic_dark",
             panels: {
               tools: {
@@ -85,7 +244,7 @@ export const ReactEmailEditor = (props: any) => {
                 collapsible: true,
               },
             },
-            // É possível customizar o spinning (verificar se é possível ocultar tbm).
+            // É possível customizar o spinning.
             loader: {
               html: '<div class="custom-spinner"></div>',
               css: ".custom-spinner { color: red; }",
@@ -98,19 +257,7 @@ export const ReactEmailEditor = (props: any) => {
               // A documentação aponto que é possível mudar o label, mas a tipagem diz que a propriedade não existe.
               // label: "teste",
             },
-            // validar como utilizar, desta forma não esta sendo aplicado.
-            // button: {
-            //   properties: {
-            //     text: {
-            //       defaultValue: "clica aee"
-            //     }
-            //   }
-            // }
-            // form: {
-
-            // },
           },
-          
         }}
       />
     </div>
